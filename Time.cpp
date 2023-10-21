56b0b26b955fcf3e0575252d2278fd03c4cbfce9
Zain <zhaldawah@gmail.com>
2023-10-21 01:57:04 -0500

    Removed all initialized values due to a calculation error which resulted from improper understanding of the functionality and use case of the variables. Setup functions for all formulas in which were used to implement calculations used to determine a specific prayer time (will not produce any results due to the fact that the initialized variables are mostly all gone)

--- Time.cpp
+++ Time.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <chrono>
 
 
 using namespace std;
 
 //Define PI
-float PI = 3.14159265359;
-
-
+double PI = 3.14159265359;
 
 //Convert degrees to radians
 double toRadians(double degrees) 
@@ -16,15 +15,17 @@ double toRadians(double degrees)
     return degrees * (PI / 180.0);
 }
 
-
-
 //Define the arcCotangent function
 double acot(double x)
 {
     return atan(1/x);
 }
 
-
+//Calculation for Sunrise/Sunset 
+double AstronomicalSSCalculation(double a, double Latitude, double Declination)
+{
+    return ((1.0 / 15.0) * acos((-sin(a) - (sin(Latitude) * sin(Declination)) / (cos(Latitude) * cos(Declination)))));
+}
 
 //Calculate Dhur prayer time
 double Dhur(double TimeZone, double Longitude, double EqT)
@@ -32,40 +33,35 @@ double Dhur(double TimeZone, double Longitude, double EqT)
     return (12 + TimeZone - (Longitude / 15) - EqT);
 }
 
-
-
 //Calculate Asr prayer time
-double Asr(double Latitude, double Declination, int ShadowLength)
+double Asr(double Latitude, double Declination, double ShadowLength)
 {
     return ((1.0/15.0) * acos((sin(acot(ShadowLength + tan(Latitude - Declination))) - (sin(Latitude) * sin(Declination))) / (cos(Latitude) * cos(Declination))));
 }
 
-
-
 int main() 
 {
-    const double ShadowLength = 1;
-    const double TimeZone = -5;
-    double d = 8692.168495;
+    const double FajrAngle = 15.0;
+    const double IshaAngle = 15.0;
+    const double ShadowLength = 1.0;
+    const double TimeZone = -5.0;
 
-    // Note: You must convert all your degrees to radians when using trigonometric functions.
-    double g = fmod((toRadians(357.529) + toRadians(0.98560028) * d), 2.0 * PI);
-    double q = fmod((toRadians(280.459) + toRadians(0.98564736) * d), 2.0 * PI);
-    double L = fmod((q + 1.915 * sin(g) + 0.020 * sin(2 * g)), 2.0 * PI);
-    double R = 1.00014 - 0.01671 * cos(g) - 0.00014 * cos(2 * g);
-    double e = toRadians(23.439 - 0.00000036 * d);
-    double RA = atan2(cos(e) * sin(L), cos(L)) / 15;  // using atan2, also removed redundancy
-    double D = asin(sin(e) * sin(L));
-    double EqT = q / 15 - RA;
 
-    // Convert RA from radians to some unit that fits the division by 15 in EqT's calculation, if necessary.
 
     cout << fixed << setprecision(5);  // for a neater output with fixed number of decimal places
-    
-
+    //Print out Fajr time
+    cout << Dhur(-5, -95.7006392, 0.12310) - ASS_Calculation(15.0, L, D) << endl;
+    //Print out Sunrise time
+    cout << Dhur(-5, -95.7006392, 0.12310) + ASS_Calculation(0.833, L, D) << endl;
     //Print out Dhur time
     cout << Dhur(-5, -95.7006392, 0.12310) << endl;
     //Print out Asr time
     cout << Dhur(-5, -95.7006392, 0.12310) + Asr(L, D, ShadowLength) << endl;
+    //Print out Sunset time
+    cout << Dhur(-5, -95.7006392, 0.12310) - ASS_Calculation(0.833, L, D) << endl;
+    //Print out Maghrib time
+    cout << Dhur(-5, -95.7006392, 0.12310) + ASS_Calculation(4.0, L, D) << endl;
+    //Print out Isha time
+    cout << Dhur(-5, -95.7006392, 0.12310) + ASS_Calculation(15.0, L, D) << endl;
     return 0;
 }
